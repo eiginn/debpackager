0bd8090596655e75285f7cc38f4e63bfda96113c
Ryan Carter <ryan@frobozzelectric.org>
2021-12-05 14:55:05 -0800

    Add go template and gomplate pkg

---
+++ pkgs/add_repo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+repo="$1"
+
+mkdir -p "$(basename "$repo")"
+
+gh repo view "$repo" --json 'name,owner,description,url,licenseInfo,latestRelease' | gomplate -d repo=stdin:///in.json -f ./_template/PKGBUILD.tmpl | tee "$(basename "$repo")/PKGBUILD"
