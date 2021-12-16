60360f58f7cebef4f606a38760074b02c0098b53
Ryan Carter <ryan@frobozzelectric.org>
2021-12-15 20:07:45 -0800

    make check_update.sh slightly better, update pkgs

--- pkgs/check_update.sh
+++ pkgs/check_update.sh
@@ -11,9 +11,19 @@ for pkgbuild in $(find "$SCRIPT_DIR" -maxdepth 2 -name PKGBUILD); do
   (
     pushd "$(dirname "$pkgbuild")" &> /dev/null || exit 1
     . "$pkgbuild"
-    latestver="$(gh release list --repo "$ghrepo" | grep Latest | awk '{print $1}')"
+    if [[ $_check_rc_prerelease == true ]]; then
+      rc_prerelease_filter=''
+    else
+      rc_prerelease_filter='| select(.tag_name | test(".*rc.*"; "i") | not)'
+    fi
+    latestver="$(gh api "repos/${ghrepo}/releases" | jq -r '[.[] '"${rc_prerelease_filter}"' | .tag_name] | .[0]')"
+    if ! [[ $latestver ]]; then
+      set -x
+      gh api "repos/${ghrepo}/releases" | jq -r '[.[] '"${rc_prerelease_filter}"' | .tag_name] | .[0]'
+      set +x
+    fi
     if [[ $pkgver != "${latestver#v}" ]]; then
-      echo "$pkgname is out of date. Newest: $latestver Have: $pkgver"
+      echo "$pkgname is out of date. Newest: ${latestver#v} Have: $pkgver"
     fi
     popd &> /dev/null || exit 1
   )
