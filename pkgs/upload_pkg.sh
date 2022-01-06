bd497a127c92355f1804e71d582f31e05a92d324
Ryan Carter <ryan@frobozzelectric.org>
2022-01-05 16:53:39 -0800

    small progress on using gcloud apt repo

---
+++ pkgs/upload_pkg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+file="$1"
+[[ -r "${file}" ]] || { echo "Can't read file: ${file}"; exit 1; }
+pkgname="$(dpkg-deb -f "${file}" Package)"
+pkgver="$(dpkg-deb -f "${file}" Version)"
+
+check_existing() {
+  gcloud artifacts versions list --repository=any --location=us-west1 --package="${pkgname}" --format=json 2> /dev/null | jq --arg pkgver "/${pkgver}$" -e '.[]|select(.name|test($pkgver))'
+}
+
+(
+  if ! check_existing; then
+    gcloud beta artifacts apt upload any --location=us-west1 --source="${file}"
+  fi
+) |& sed -e 's/projects\/.*\/locations/projects\/REMOVED\/locations/g'
