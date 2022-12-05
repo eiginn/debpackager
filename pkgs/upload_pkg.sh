c93d056df3b0f7299e09daa562746fefd05c4445
Ryan Carter <ryan@frobozzelectric.org>
2022-12-05 12:42:16 -0800

    updates

--- pkgs/upload_pkg.sh
+++ pkgs/upload_pkg.sh
@@ -6,12 +6,12 @@ pkgname="$(dpkg-deb -f "${file}" Package)"
 pkgver="$(dpkg-deb -f "${file}" Version)"
 
 check_existing() {
-  gcloud artifacts versions list --repository=any --location=us-west1 --package="${pkgname}" --format=json 2> /dev/null | jq --arg pkgver "/${pkgver}$" -e '.[]|select(.name|test($pkgver))'
+  gcloud artifacts versions list --repository=jammy-amd64 --location=us-west1 --package="${pkgname}" --format=json 2> /dev/null | jq --arg pkgver "/${pkgver}$" -e '.[]|select(.name|test($pkgver))'
 }
 
 (
   echo "Uploading: ${file}..."
   if ! check_existing; then
-    gcloud beta artifacts apt upload any --location=us-west1 --source="${file}"
+    gcloud beta artifacts apt upload jammy-amd64 --location=us-west1 --source="${file}"
   fi
 ) |& sed -e 's/projects\/.*\/locations/projects\/REMOVED\/locations/g'
