dc71c61d7ff9888b7ce43cdfb6b1a1e43858cbf3
Ryan Carter <eiginn@users.noreply.github.com>
2024-12-13 13:41:09 -0800

    updates

--- pkgs/add_repo.sh
+++ pkgs/add_repo.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 repo="$1"
 
-mkdir -p "$(basename "$repo")"
+mkdir -p "${SCRIPT_DIR}/$(basename "$repo")"
 
-gh repo view "$repo" --json 'name,owner,description,url,licenseInfo,latestRelease' | gomplate -d repo=stdin:///in.json -f ./_template/PKGBUILD.ttmpl | tee "$(basename "$repo")/PKGBUILD"
+gh repo view "$repo" --json 'name,owner,description,url,licenseInfo,latestRelease' | gomplate -d repo=stdin:///in.json -f "${SCRIPT_DIR}/_template/PKGBUILD.ttmpl" | tee "${SCRIPT_DIR}/$(basename "$repo")/PKGBUILD"
